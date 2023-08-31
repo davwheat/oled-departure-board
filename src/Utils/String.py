9dba8ccf641853a7c2ecccadba8bc24d08c0abc8
David Wheatley <david@davwheat.dev>
2023-08-31 23:58:41 +0000

    feat: add ordinal util

--- src/Utils/String.py
+++ src/Utils/String.py
@@ -3,3 +3,16 @@ def pluralise(snippets: list[str]) -> str:
         return snippets[0]
     else:
         return ", ".join(snippets[:-1]) + " and " + snippets[-1]
+
+
+def getOrdinal(num: int) -> str:
+    last = num % 10
+
+    if last == 1:
+        return f"{num}st"
+    elif last == 2:
+        return f"{num}nd"
+    elif last == 3:
+        return f"{num}rd"
+    else:
+        return f"{num}th"
